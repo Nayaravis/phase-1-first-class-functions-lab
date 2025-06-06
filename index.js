f26b2c9c3959602b83eea5c643844d32fed243e8
Navis Ochieng <navis.ochieng@student.moringaschool.com>
2025-06-06 15:36:26 +0300

    lab completed

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Code your solution in this file!
+const returnFirstTwoDrivers = (function (drivers) {
+    return drivers.slice(0, 2)
+})
+
+const returnLastTwoDrivers = (function (drivers) {
+    return drivers.slice(drivers.length-2, drivers.length)
+})
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+function createFareMultiplier(someValue) {
+    function multiplier(fare) {
+        return fare * someValue
+    }
+
+    return multiplier
+}
+
+const fareDoubler = (fare) => createFareMultiplier(2)(fare)
+const fareTripler = (fare) => createFareMultiplier(3)(fare)
+
+function selectDifferentDrivers(arrayOfDrivers, callback) {
+    return callback(arrayOfDrivers)
+}
